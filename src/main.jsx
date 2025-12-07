ef08d99f3750252c1e51755aba0f9c0036920354
Sushil Pradhan <sushil.frontenddev@gmail.com>
2025-12-07 19:53:17 +0545

    Add CSS Architecture and styling system

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,12 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { App } from "./App";
 
+import "./styles/base.css";
+import "./styles/layout.css";
+import "./styles/dashboard.css";
+import "./styles/admin-layout.css";
+import "./styles/components.css";
+
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
