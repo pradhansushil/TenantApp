65d63d08ad9bbcc721830edcb7ba5ddb552347c0
Sushil Pradhan <sushil.frontenddev@gmail.com>
2025-12-10 20:37:33 +0545

    Add modals.css to Main

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,10 @@ import "./styles/layout.css";
 import "./styles/dashboard.css";
 import "./styles/admin-layout.css";
 import "./styles/components.css";
+import "./styles/tables.css";
+import "./styles/forms.css";
+import "./styles/modals.css";
+import "./styles/filter.css";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
